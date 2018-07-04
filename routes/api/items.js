26932017c307028e851640be8b4f1e70f74ddacd
Nick Stellato <nick@nickstellato.com>
2018-07-04 00:49:14 -0500

    Prettier reformatted

--- routes/api/items.js
+++ routes/api/items.js
@@ -30,8 +30,12 @@ router.post("/", (req, res) => {
 // @access Public
 router.delete("/:id", (req, res) => {
   Item.findById(req.params.id)
-    .then(item => item.remove().then(() => res.json({ success: true })))
-    .catch(err => res.status(404).json({ success: false }));
+    .then(item => item.remove().then(() => res.json({
+      success: true
+    })))
+    .catch(err => res.status(404).json({
+      success: false
+    }));
 });
 
 module.exports = router;
